a001107fc3e81931712a3343d98988e3ab6b5d54
Ryan Enderby <rae83@cornell.edu>
2018-05-25 00:30:35 -0400

    wrote function to return completions from a given node

--- preprocess.py
+++ preprocess.py
@@ -11,13 +11,14 @@ class TrieNode():
     def __init__(self, char: str):
         self.char = char
         self.children = []
-        self.end_of_sentence = False
+        self.is_end_of_sentence = False
 
 
 class Trie():
 
     def __init__(self, root: TrieNode):
         self.root = root
+
 
     def add_sentence(self, root: TrieNode, sentence: list):
 
@@ -28,8 +29,8 @@ class Trie():
             # if the leading char in the string is already a child, no need to add a new node
             if character == child.char:
 
-                if len(sentence) == 1: # is this block correct?
-                    child.end_of_sentence = True
+                if len(sentence) == 1:
+                    child.is_end_of_sentence = True
                     return self
 
                 return self.add_sentence(child, sentence[1:])
@@ -37,24 +38,43 @@ class Trie():
         node = TrieNode(character)
 
         if len(sentence) == 1:
-            node.end_of_sentence = True
+            node.is_end_of_sentence = True
             root.children.append(node)
             return self
 
         root.children.append(node)
         return self.add_sentence(node, sentence[1:])
+    
+
+    def return_completions_from_node(self, node: TrieNode):
+
+        def enumerate_sentences(node: TrieNode, sentence: str, sentences: list):
+            
+            if len(node.children) > 0:
+                for child in node.children:
+                    sentence += child.char
+                    if child.is_end_of_sentence:
+                        sentences.append(sentence)
+                    enumerate_sentences(child, sentence, sentences)
+            
+            return sentences
+
+        return enumerate_sentences(node, "", [])
+
 
     def contains(self, root: TrieNode, sentence: list):
-
+        """
+        Returns (True, last node visited) if a sentence exists in the trie, (False, None) otherwise
+        """
         character = sentence[0]
 
         for child in root.children:
             if character == child.char:
                 if len(sentence) == 1:
-                    return True
+                    return (True, child)
                 return self.contains(child, sentence[1:])
 
-        return False
+        return (False, None)
 
 
 def extract_sentences_from_json(file_path: str):
@@ -63,13 +83,12 @@ def extract_sentences_from_json(file_path: str):
 
     with open(file_path) as json_data:
         data = json.load(json_data)
-    
+
     for issues in data["Issues"]:
         for message in issues["Messages"]:
             sentences.append(message["Text"])
-    
-    return sentences
 
+    return sentences
 
 
 if __name__ == "__main__":
@@ -78,11 +97,20 @@ if __name__ == "__main__":
     root = TrieNode('')
     trie = Trie(root)
 
-    sentences = extract_sentences_from_json("sample_conversations.json") # TODO: pickle this so it can be reloaded instead of recomputed each time
-    
+    sentences = extract_sentences_from_json("sample_conversations.json")
+
     for sentence in sentences:
         trie.add_sentence(root, sentence)
+    
+    # TODO: pickle the trie so it can be reloaded instead of recomputed each time
 
-    print(trie.contains(root, "Hi! I placed an order on your website and I can't find the tracking number. Can you help me find out where my package is"))
-    print(trie.contains(root, "Hi! I placed an order on your website and I can't find the tracking number. Can you help me find out where my package is?"))
-    print(trie.contains(root, "Hi! I placed an order on your NOT website and I can't find the tracking number. Can you help me find out where my package is?"))
+    (boolean_1, node_1) = trie.contains(root, "Hi! I placed an order on your website and I can't find the tracking number. Can you help me")
+    (boolean_2, node_2) = trie.contains(root, "Hi")
+    (boolean_3, node_3) = trie.contains(root, "Hi! I placed an order on your NOT website and I can't find the tracking number. Can you help me find out where my package is?")
+
+    print(boolean_1, node_1)
+    print(boolean_2, node_2)
+    print(boolean_3, node_3)
+
+    print(trie.return_completions_from_node(node_1))
+    print(trie.return_completions_from_node(node_2))
