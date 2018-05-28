415c4c4f61c0baeb5869017a173391d713812a93
Ryan Enderby <rae83@cornell.edu>
2018-05-27 23:19:42 -0400

    rnn now autocompletes as necessary

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import os
 import pickle
 from keras.models import load_model
 from trie import Trie, TrieNode, extract_sentences_from_json, save_sentences_to_file, initialize_prefix_trie
-from rnn import generate_text
+from rnn import generate_text, build_inference_model
 
 
 class autocomplete_handler(tornado.web.RequestHandler):
@@ -20,14 +20,17 @@ class autocomplete_handler(tornado.web.RequestHandler):
         if contains:
             return trie.return_completions_from_node(node, prefix=prefix)[:n]
         else:
-            return generate_text(model, prefix)
+            completions = []
+            for _ in range(n):
+                completions.append(generate_text(model, prefix))
+            return completions
 
     def get(self):
         """
         Parse args from URL and return autocompletions as JSON.
         """
         args = self.get_arguments("q")[0]
-        response = {"Completions": self.autocomplete(trie, rnn, args)}
+        response = {"Completions": self.autocomplete(trie, inference_model, args)}
         self.write(response)
         self.write('\n')
 
@@ -44,7 +47,12 @@ def make_app():
 if __name__ == "__main__":
 
     trie = initialize_prefix_trie()
-    rnn = load_model("rnn/checkpoints/model.ckpt")
+
+    # Load learning model for config and weights
+    model = load_model("checkpoints/model.ckpt")
+    # Build inference model and transfer weights
+    inference_model = build_inference_model(model)
+    inference_model.set_weights(model.get_weights())
 
     # Start the server
     app = make_app()
